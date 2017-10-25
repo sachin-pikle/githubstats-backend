09adc91f115a972f512d5e92eef1123623733800
sachinpikle <sachin.pikle@gmail.com>
2017-10-25 06:50:31 -0700

    Initial release

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+kubectl delete deployment githubstats-v2
+kubectl delete deployment githubstats-v3
+kubectl delete deployment activitymap-v2
+kubectl delete deployment activitymap-v3
+istioctl replace -f mesh/githubstats-v2-routing.yaml
+istioctl replace -f mesh/map-v2-routing.yaml
+kubectl delete svc activitymap
+kubectl delete svc githubstats
+
